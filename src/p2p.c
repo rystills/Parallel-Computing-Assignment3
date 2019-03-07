83e50b5bd02947b2c086c64eaed88900d7a433b6
Ryan Stillings <rystills@gmail.com>
2019-03-07 18:38:28 -0500

    add provided code with minor tweaks, updated assignment doc

--- src/p2p.c
+++ src/p2p.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#define BGQ 1 // when running BG/Q, comment out when running on mastiff
+#include <mpi.h>
+#ifdef BGQ
+#include<hwi/include/bqc/A2_inlines.h>
+#else
+#define GetTimeBase MPI_Wtime
+#endif
+double processor_frequency = 1600000000.0;
 
-int main(void) {
-	puts("hello World");
-	return EXIT_SUCCESS;
+void MPI_P2P_Reduce() {
+}
+
+int main(int argc, char* argv[]) {
+	unsigned long long start_cycles = GetTimeBase();
+	MPI_P2P_Reduce();
+	unsigned long long end_cycles = GetTimeBase();
+	double time_in_secs = ((double)(end_cycles - start_cycles)) / processor_frequency;
+
 }
