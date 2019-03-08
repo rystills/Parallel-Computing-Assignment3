7758da1bfc088bec06c88d9a95d031deb38ae0e9
Ryan Stillings <rystills@gmail.com>
2019-03-08 01:09:39 -0500

    proper time measurement, minor collective reduce refactoring

--- src/p2p.c
+++ src/p2p.c
@@ -7,22 +7,23 @@
 #include <math.h>
 #ifdef BGQ
 #include<hwi/include/bqc/A2_inlines.h>
+#define processor_frequency 1600000000.0
 #else
 #define GetTimeBase MPI_Wtime
+#define processor_frequency 1.0
 #endif
 #define DEBUG false
 #define numEntries 1073741824
 //#define numEntries 32768
 
 //MPI data
-double processor_frequency = 1600000000.0;
 int numRanks = -1; //total number of ranks in the current run
 int rank = -1; //our rank
 int elementsPerProc = -1; //stores how many elements each process is responsible for
 
 void MPI_P2P_Reduce() {
 	//only rank needs to write the output
-	if (rank == 0) {
+	if (!rank) {
 		if (!DEBUG) freopen("STDOUT_1.txt", "w", stdout);
 		printf("%llu\n",(unsigned long long)(pow(2,29)*(pow(2,30)-1)));
 	}
@@ -31,7 +32,7 @@ void MPI_P2P_Reduce() {
 int main(int argc, char* argv[]) {
 	//usage check
 	if (argc != 1) {
-		fprintf(stderr,"Error: %d input argument[s] supplied, but 0 were expected. Usage: mpirun -np X ./p2p.out\n",argc-1);
+		fprintf(stderr,"Error: %d input argument[s] were supplied, but 0 were expected. Usage: mpirun -np X ./p2p.out\n",argc-1);
 		exit(1);
 	}
 
@@ -41,30 +42,31 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	elementsPerProc = numEntries/(float)numRanks;
 
-	//execute p2p reduce
+	//populate deterministic input data
+	unsigned long *inputData = malloc(elementsPerProc * sizeof(unsigned long));
+	for (unsigned long i = 0; i < elementsPerProc; ++i) {
+		inputData[i] = rank*elementsPerProc + i;
+	}
+
+	//execute point to point reduction
 	unsigned long long start_cycles = GetTimeBase();
 	MPI_P2P_Reduce();
-	//now compare to collective reduce
-	unsigned long *send_data = malloc(elementsPerProc * sizeof(unsigned long));
-	for (unsigned long i = 0; i < elementsPerProc; ++i) {
-		send_data[i] = rank*elementsPerProc + i;
-	}
-	unsigned long *recv_data = malloc(rank==0?elementsPerProc * sizeof(unsigned long):0);
-	MPI_Reduce(send_data, recv_data, elementsPerProc, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-	if (rank == 0) {
+	if (DEBUG && !rank) fprintf(stderr, "P2P Execution time: %fs\n",(GetTimeBase() - start_cycles) / processor_frequency);
+
+	//execute collective reduction
+	start_cycles = GetTimeBase();
+	unsigned long *recv_data = malloc(!rank ? elementsPerProc * sizeof(unsigned long) : 0);
+	MPI_Reduce(inputData, recv_data, elementsPerProc, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+	//only rank 0 needs to sum the final array and write the output
+	if (!rank) {
 		unsigned long long sum = 0;
-		for (unsigned long i = 0; i < elementsPerProc; ++i) {
-			sum += recv_data[i];
-		}
-		//only rank 0 needs to write the output
+		for (unsigned long i = 0; i < elementsPerProc; sum += recv_data[i++]);
 		printf("%llu\n",sum);
-		if (DEBUG) {
-			fprintf(stderr, "Execution time: %f\n",((double)(GetTimeBase() - start_cycles)) / processor_frequency);
-		}
+		if (DEBUG) fprintf(stderr, "COL Execution time: %fs\n",(GetTimeBase() - start_cycles) / processor_frequency);
 	}
 
 	//all done
 	MPI_Finalize();
-	free(send_data);
+	free(inputData);
 	free(recv_data);
 }
