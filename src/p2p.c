1c5955a09682f9e5f1f885e63736a3e49a8fecc0
Ryan Stillings <rystills@gmail.com>
2019-03-09 14:49:54 -0500

    implement point to point reduce operation

--- src/p2p.c
+++ src/p2p.c
@@ -14,18 +14,42 @@
 #endif
 #define DEBUG false
 #define numEntries 1073741824
-//#define numEntries 32768
 
 //MPI data
 int numRanks = -1; //total number of ranks in the current run
 int rank = -1; //our rank
 int elementsPerProc = -1; //stores how many elements each process is responsible for
 
-void MPI_P2P_Reduce() {
-	//only rank needs to write the output
-	if (!rank) {
-		if (!DEBUG) freopen("STDOUT_1.txt", "w", stdout);
-		printf("%llu\n",(unsigned long long)(pow(2,29)*(pow(2,30)-1)));
+/**
+ * apply a point to point reduction on the send_data, storing the final result in recv_data
+ * @param send_data: array of elements of type datatype that we are going to reduce
+ * @param recv_data: element of type datatype in which we will store the reduction result
+ * @param count: number of elements in send_data
+ * @param datatype: data type of elements in send_data (hard-coded unsigned long as per spec)
+ * @param op: operation to be performed (hard-coded MPI_SUM as per spec)
+ * @param root: the process on which the final value will be stored (hard-coded rank 0 as per spec)
+ * @param communicator: process grouping communicator to use (hard-coded MPI_COMM_WORLD as per spec)
+ */
+void MPI_P2P_Reduce(unsigned long* send_data, unsigned long long* recv_data, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm communicator) {
+	//calculate this rank's sum
+	*recv_data = 0;
+	for (unsigned long i = 0; i < count; *recv_data += send_data[i++]);
+	//pairwise add across ranks
+	MPI_Request request;
+	MPI_Status status;
+	unsigned long long buffer;
+	for (int i = 1; i <= numRanks/2; i*=2) {
+		//receiver
+		if (rank % (i*2) == 0) {
+			MPI_Irecv(&buffer, 1, MPI_UNSIGNED_LONG_LONG, rank+i, 0, MPI_COMM_WORLD, &request);
+		}
+		//sender
+		else {
+			MPI_Isend(recv_data, 1, MPI_UNSIGNED_LONG_LONG, rank-i, 0, MPI_COMM_WORLD, &request);
+			return;
+		}
+		MPI_Wait(&request, &status);
+		*recv_data += buffer;
 	}
 }
 
@@ -47,21 +71,27 @@ int main(int argc, char* argv[]) {
 	for (unsigned long i = 0; i < elementsPerProc; ++i) {
 		inputData[i] = rank*elementsPerProc + i;
 	}
+	unsigned long *recv_data = malloc(!rank ? elementsPerProc * sizeof(unsigned long) : 0);
+	unsigned long long recv_sum = -1;
+	if (!DEBUG) freopen("STDOUT_1.txt", "w", stdout);
 
 	//execute point to point reduction
 	unsigned long long start_cycles = GetTimeBase();
-	MPI_P2P_Reduce();
-	if (DEBUG && !rank) fprintf(stderr, "P2P Execution time: %fs\n",(GetTimeBase() - start_cycles) / processor_frequency);
+	MPI_P2P_Reduce(inputData, &recv_sum, elementsPerProc, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+	//only rank 0 needs to write the output
+	if (!rank) {
+		printf("%llu%s",recv_sum, DEBUG ? " " : "\n");
+		if (DEBUG) fprintf(stderr, "P2P Execution time: %fs\n",(GetTimeBase() - start_cycles) / processor_frequency);
+	}
 
 	//execute collective reduction
 	start_cycles = GetTimeBase();
-	unsigned long *recv_data = malloc(!rank ? elementsPerProc * sizeof(unsigned long) : 0);
 	MPI_Reduce(inputData, recv_data, elementsPerProc, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 	//only rank 0 needs to sum the final array and write the output
 	if (!rank) {
 		unsigned long long sum = 0;
 		for (unsigned long i = 0; i < elementsPerProc; sum += recv_data[i++]);
-		printf("%llu\n",sum);
+		printf("%llu%s",sum, DEBUG ? " " : "\n");
 		if (DEBUG) fprintf(stderr, "COL Execution time: %fs\n",(GetTimeBase() - start_cycles) / processor_frequency);
 	}
 
